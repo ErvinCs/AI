47c1f0b53ffe3b9656d896536f4b9d0cbefa30c8
ErvinCs <csoka.ervin@gmail.com>
2018-08-16 00:32:50 +0300

    Lab2-3 - EA & ACO - Pyramid Building

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
-from src.domain.Problem import Problem
-from src.domain.State import State
-from src.controller.GameController import GameControler
-from src.ui.TextUI import TextUI
+from src.UI.EAUI import EAUI
+from src.UI.ACOUI import ACOUI
+'''
+Consider n cubes of known sides’ length si and colors ci . Assemble the highest
+pyramid from the cubes in such a way that it has ‘stability’ (there is not a bigger cube
+over a smaller one) and there are not two consecutive cubes of the same color.
+'''
+
 if __name__ == '__main__':
-    ui = TextUI()
-    ui.mainMenu()
+    while True:
+        userInterface = input("Algorithm: ")
+        if userInterface == '1':
+            ui = EAUI()
+            ui.run()
+            break
+        elif userInterface == '2':
+            ui = ACOUI()
+            ui.run()
+            break
